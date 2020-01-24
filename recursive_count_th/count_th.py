6c6b67213491b624ce56e3c12f946ace79bc6d57
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-24 10:49:18 -0600

    mapped out count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,8 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
+    # if the word is too short, return 0 (set base)
+    # if the first two letters are 'th', return 1 + recursive search at index to the right of 1
+    # otherwise return recursive search at index to the right of 1
     
-    pass
+    
