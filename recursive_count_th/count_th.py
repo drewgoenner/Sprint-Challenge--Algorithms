ce149951fddea2459921ec3161a9d8eeabce6084
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2020-01-24 11:13:35 -0600

    completed counting_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,7 +7,17 @@ def count_th(word):
     
     # TBC
     # if the word is too short, return 0 (set base)
-    # if the first two letters are 'th', return 1 + recursive search at index to the right of 1
+    if len(word) < 2:
+    	return 0
+    # if the first two letters are 'th', return 1 + recursive search at index to the
+    if word[0] == 't' and word[1] == 'h':
+    	return 1 + count_th(word[1:])
     # otherwise return recursive search at index to the right of 1
+    else:
+    	return count_th(word[1:])
+    # return the count
+    return(count_th)
+    
+    
     
     
